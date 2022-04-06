7acd9aec8d1b07162f867c20856af42e9498f177
geo-petrini <geo.petrini@gmail.com>
2022-04-06 23:58:35 +0200

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 def setup_db(app):    
     database_name ='local_db_name'
-    default_database_path= "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', database_name)
+    default_database_path= "postgresql://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', database_name)
     database_path = os.getenv('DATABASE_URL', default_database_path)
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
